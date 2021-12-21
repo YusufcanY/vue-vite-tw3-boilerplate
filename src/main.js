bcbc9cf2c390ca8d4883859af3fa77e04b0a2424
Yusufcan Yilmaz <ycanny1@gmail.com>
2021-12-21 11:44:19 +0300

    Vue Router Next added  to project

--- src/main.js
+++ src/main.js
@@ -1,17 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import './assets/css/tailwind.css';
-import VueRouter from "vue-router";
-
-import Home from './views/Home.vue';
-
-const routes = [
-    { path: '/', component: Home },
-];
-  
-const router = VueRouter.createRouter({
-    history: VueRouter.createWebHashHistory(process.env.BASE_URL),
-    routes,
-});
+import router from '@/router';
 
 createApp(App).use(router).mount('#app');

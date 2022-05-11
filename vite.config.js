7031be414675f223afeb43c644061e9fef1c4d74
Yusufcan Yilmaz <ycanny1@gmail.com>
2022-05-11 18:17:15 +0300

    refac: vite pwa plugin deleted

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { VitePWA } from 'vite-plugin-pwa'
 import eslint from 'vite-plugin-eslint'
 import svg from 'vite-svg-loader'
 
 export default defineConfig({
-  plugins: [vue(), eslint(), svg(), VitePWA()],
+  plugins: [vue(), eslint(), svg()],
   resolve: {
     alias: [
       {

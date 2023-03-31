6ebee2d0ce3a5fb5caac87610338574775eed103
Yusufcan Yilmaz <playders03@gmail.com>
2023-04-01 00:58:33 +0300

    Added Reactivity Transform from vue-macros and implemented

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import eslint from 'vite-plugin-eslint'
 import svg from 'vite-svg-loader'
+import ReactivityTransform from '@vue-macros/reactivity-transform/vite'
 
 export default defineConfig({
-  plugins: [vue(), eslint(), svg()],
+  plugins: [vue(), eslint(), svg(), ReactivityTransform()],
   resolve: {
     alias: [
       {
